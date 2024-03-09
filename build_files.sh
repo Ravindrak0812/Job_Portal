ca21a5a8686012c494a2c63dfafafaac1bff1a8d
Ravindra Kumar <ravindra8122003@gmail.com>
2024-03-09 17:30:04 +0530

    done vercel setting

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noimput --clear
+python3.10 -m pip install -r requirements.txt
+python3.10 manage.py collectstatic --noimput --clear
 echo "BUILD END"
