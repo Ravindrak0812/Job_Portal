f6db6db53a5fa0a9e45eda5a2f1c9c9ba10c54d3
Ravindra Kumar <107619088+Ravindrak0812@users.noreply.github.com>
2024-03-11 11:44:11 +0530

    Update settings.py

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -79,15 +79,10 @@ WSGI_APPLICATION = 'job_portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'OGDkmoQhovhcFbrqanyuXyRnXaMFxlrw',
-        'HOST': 'viaduct.proxy.rlwy.net',
-        'PORT': '29282',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
