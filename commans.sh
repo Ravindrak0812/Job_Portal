592c3e6037b9f5246934d6fb835d6862b893b7af
Ravindra Kumar <ravindra8122003@gmail.com>
2024-03-14 16:45:17 +0530

    y

--- commans.sh
+++ commans.sh
@@ -1,21 +1,11 @@
-# shellcheck disable=SC1073
-# shellcheck disable=SC1035
-# shellcheck disable=SC1072
-# shellcheck disable=SC1020
-[Unit]
-Description=gunicorn daemon
-Requires=gunicorn.socket
-After=network.target
+server {
+    listen 80;
+    server_name 13.49.78.116;
 
-[Service]
-User=ubuntu
-Group=www-data
-WorkingDirectory=/home/ubuntu/project/Job_Portal
-ExecStart=/home/ubuntu/project/env/bin/gunicorn \
-          --access-logfile - \
-          --workers 3 \
-          --bind unix:/run/gunicorn.sock \
-          job_portal.wsgi:application
 
- [Install]
-WantedBy=multi-user.target
+    location=/favicon.ico {access_log off; log_not_found off; }
+    location / {
+        include proxy_params;
+        proxy_pass http://unix:/run/gunicorn.sock;
+    }
+}
