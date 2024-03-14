099d9ab688e3b46ec17e57e4f892daa6feb405b5
Ravindra Kumar <ravindra8122003@gmail.com>
2024-03-14 16:51:40 +0530

    changes settings.py

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -131,5 +131,4 @@ MESSAGE_TAGS = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
