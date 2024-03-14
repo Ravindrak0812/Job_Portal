42497327b5e7eb13a4e58bf5f4c62d1d8632621b
Ravindra Kumar <ravindra8122003@gmail.com>
2024-03-14 14:20:07 +0530

    changes in navbar

--- job_portal/urls.py
+++ job_portal/urls.py
@@ -2,11 +2,9 @@ from django.contrib import admin
 from django.template.context_processors import static
 from django.urls import path, include
 
-from job_portal.job_portal import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('job.urls'))
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
